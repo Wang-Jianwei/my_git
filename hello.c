a30c8ce418d5c1031d7a8e86654e88da694dd67e
Away <3152974246@qq.com>
2018-03-12 11:13:50 +0800

    changed the hello.c

--- hello.c
+++ hello.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
+struct book
+{
+	char *name;
+	float price;
+	unsigned ISBN;
+};
+
+
 int main(int argc, char const *argv[])
 {
-	printf("hello world\n");
-	int *p;
-	int *p8;
-	int a;
-	a = 100;
-	int *lmp;
-	lmp = &a;
-	*lmp = 200;
-	printf("%d\n", a);
+	struct book math = {
+		.name = "math",
+		.price = 35,
+		.ISBN = 9547
+	}; 
+	
+	struct book class[60];
+
+	struct book *p = &math;
+	math.price = 25;
+	p->price = 38;
+
+		
 	return 0;
 }
